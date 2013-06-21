16d99bd5d34eb5d5ac708581d8d5433f24affc61
VincentRaia <vsraia@gmail.com>
2013-06-21 00:27:25 -0400

    init commit

---
+++ spread_analyzer.py
@@ -0,0 +1,24 @@
+"""
+This will be used for testing pairs
+
+Function                        Output
+------------------------------------------
+Random Walk?                    True/False
+Cointegration Level:            0-100%
+Pearson Correlation (Price):    0-100%
+Beta (each other):              Beta
+Current Spread:                 Current Spread
+Dollar-Neutral Ratio:           Current Ratio
+Average Price Ratio:            Average Ratio
+Beta 1                          Beta
+Beta 2                          Beta
+Spread Mean:                    Spread Mean
+Spread Median:                  Spread Maximum
+Spread Maximum:                 Spread Maximum
+Spread Minimum:                 Spread Minimum
+Half-life:                      Half-life
+Current z-score                 Current z-score
+$ value of 1 z-score            value
+"""
+
+def get_data(p1,p2,start,end):
